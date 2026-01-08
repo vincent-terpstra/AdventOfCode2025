a78841f459ac15b0bfe3ac6d1e71be79dfc82b0e
Vincent Terpstra <vdterpst@gmail.com>
2026-01-07 23:39:53 -0500

    Working towards solution for Part 9 using scaled map

--- Day_7/Models/Map.cs
+++ Day_7/Models/Map.cs
@@ -8,4 +8,21 @@ public class Map
     {
         Values = input.Select(l => l.ToList()).ToList();
     }
+
+    public Map(int width, int height, char initial = '.')
+    {
+        Values = new List<List<char>>(height);
+        for (int i = 0; i < height; i++)
+        {
+            Values.Add(new List<char>(width));
+            for (int j = 0; j < width; j++) 
+                Values[i].Add(initial);
+        }
+    }
+
+    public void Display(Action<string> display)
+    {
+        foreach (var line in Values) 
+            display(string.Join("", line));
+    }
 }
--- Day_9/MovieTheaterSolution.cs
+++ Day_9/MovieTheaterSolution.cs
@@ -1,16 +1,10 @@
-﻿using Xunit.Abstractions;
+﻿using AOC_2025.Day_7.Models;
+using Xunit.Abstractions;
 
 namespace AOC_2025.Day_9;
 
-public class MovieTheaterSolution
+public class MovieTheaterSolution(ITestOutputHelper testOutputHelper)
 {
-    private readonly ITestOutputHelper _testOutputHelper;
-
-    public MovieTheaterSolution(ITestOutputHelper testOutputHelper)
-    {
-        _testOutputHelper = testOutputHelper;
-    }
-
     [Fact]
     public void Demo_Input_Should()
     {
@@ -34,9 +28,108 @@ public class MovieTheaterSolution
             }
         }
         
-        _testOutputHelper.WriteLine(maxArea.ToString());
+        testOutputHelper.WriteLine(maxArea.ToString());
         Assert.Equal(50, maxArea);
         // Assert.Equal(expected, actual)
+    }
+
+    [Fact]
+    public void Demo_Input_Red_WithFill()
+    {
+        var input = FileHelper.ReadFileAsList(9, "puzzle.txt");
+        var coordinates = Coordinate.FromFile(input);
+        
+       // var map = new CoordinateMap(12, 12);
+
+        // for (int i = 0; i < coordinates.Count; i++)
+        // {
+        //     var current = coordinates[i];
+        //     var next = coordinates[(i + 1) % coordinates.Count];
+        //     map.DrawLine(current, next);
+        // }
+        //
+        // // map.Fill('X');
+        //
+        // map.Display(testOutputHelper.WriteLine);
+        
+        var orderedX = coordinates.Select(c => c.X).Order().Distinct().ToList();
+        var orderedY = coordinates.Select(c => c.Y).Order().Distinct().ToList();
+        
+        var xDict = orderedX.Select((x, i) => (x, i)).ToDictionary(x => x.x, x => x.i + 1);
+        var yDict = orderedY.Select((x, i) => (x, i)).ToDictionary(x => x.x, x => x.i + 1);
+
+        var scaleMap = new CoordinateMap(orderedX.Count + 2, orderedY.Count + 2);
+
+        for (int i = 0; i < coordinates.Count; i++)
+        {
+            var current = coordinates[i];
+            var next = coordinates[(i + 1) % coordinates.Count];
+            
+            var x1 = xDict[current.X];
+            var y1 = yDict[current.Y];
+            var x2 = xDict[next.X];
+            var y2 = yDict[next.Y];
+            
+            scaleMap.DrawLine(x1, y1, x2, y2);
+        }
+        
+        scaleMap.Fill(' ');
+        
+        scaleMap.Display(testOutputHelper.WriteLine);
+    }
+}
+
+public class CoordinateMap(int width, int height) : Map(width, height)
+{
+    public void SetValue(int x, int y, char value)
+    {
+        base.Values[x][y] = value;
+    }
+
+    public void DrawLine(int x1, int y1, int x2, int y2)
+    {
+        int dx = (int)Math.Sign(x2 - x1);
+        int dy = (int)Math.Sign(y2 - y1);
+        
+        int x = x1;
+        int y = y1;
+        
+        SetValue(x, y, '#');
+        while (x != x2 || y != y2)
+        {
+            x += dx;
+            y += dy;
+            SetValue(x, y, '#');
+        }
+    }
+
+    public void DrawLine(Coordinate a, Coordinate b)
+    {
+        DrawLine((int)a.X, (int)a.Y,(int)b.X, (int)b.Y);
+    }
+
+    public void Fill(char set)
+    {
+        List<(int x, int y)> queue = new();
+        int width = Values.Count;
+        int height = Values[0].Count;
+        queue.Add((0, 0));
+
+        while (queue.Count > 0)
+        {
+            var (x, y) = queue.First();
+            queue.RemoveAt(0);
+            
+            // check boundary
+            if (x >= 0 && y >= 0 && x < width && y < height && Values[x][y] == '.')
+            {
+                SetValue(x, y, set);
+                queue.Add((x - 1, y));
+                queue.Add((x + 1, y));
+                queue.Add((x, y - 1));
+                queue.Add((x, y + 1));
+            }
+        }
     }
 }
 
